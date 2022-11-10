3d41d34c95da4a3812bd5de0000107fd026afefd
Xavier Ripoll <xaviripo97@gmail.com>
2022-11-10 20:28:09 +0100

    Homework Sheet 3

--- lean/love03_forward_proofs_homework_sheet.lean
+++ lean/love03_forward_proofs_homework_sheet.lean
@@ -27,15 +27,33 @@ namespace backward_proofs
 
 lemma peirce_of_dn :
   double_negation → peirce :=
-sorry
+fix hdn : double_negation,
+have hem : excluded_middle :=
+  sorry_lemmas.em_of_dn hdn,
+have hp : peirce :=
+  peirce_of_em hem,
+show peirce, from
+  hp
 
 lemma em_of_peirce :
   peirce → excluded_middle :=
-sorry
+fix hp : peirce,
+have hdn : double_negation :=
+  dn_of_peirce hp,
+have hem : excluded_middle :=
+  sorry_lemmas.em_of_dn hdn,
+show excluded_middle, from
+  hem
 
 lemma dn_of_em :
   excluded_middle → double_negation :=
-sorry
+fix hem : excluded_middle,
+have hp : peirce :=
+  peirce_of_em hem,
+have hdn : double_negation :=
+  dn_of_peirce hp,
+show double_negation, from
+  hdn
 
 end backward_proofs
 
@@ -43,9 +61,23 @@ end backward_proofs
 an `∃` quantifier, using no other lemmas than the introduction and elimination
 rules for `∃`, `∧`, and `↔`. -/
 
+lemma exists_and_commute_aux {α : Type} (p q : α → Prop) :
+  (∃x, p x ∧ q x) → (∃x, q x ∧ p x) :=
+assume he,
+show ∃x, q x ∧ p x, from exists.elim he (
+  fix a,
+  assume hpq,
+  have hp : p a, from and.elim_left hpq,
+  have hq : q a, from and.elim_right hpq,
+  have hqp : q a ∧ p a, from and.intro hq hp,
+  show ∃ (x : α), q x ∧ p x, from exists.intro a hqp
+)
+
 lemma exists_and_commute {α : Type} (p q : α → Prop) :
   (∃x, p x ∧ q x) ↔ (∃x, q x ∧ p x) :=
-sorry
+have ltr : (∃x, p x ∧ q x) → (∃x, q x ∧ p x), from exists_and_commute_aux p q,
+have rtl : (∃x, q x ∧ p x) → (∃x, p x ∧ q x), from exists_and_commute_aux q p,
+show (∃x, p x ∧ q x) ↔ (∃x, q x ∧ p x), from iff.intro ltr rtl
 
 
 /-! ## Question 2 (3 points): Fokkink Logic Puzzles
@@ -75,13 +107,24 @@ Hint: There is an easy way. -/
 
 lemma weak_peirce₂ :
   ∀a b : Prop, ((((a → b) → a) → a) → b) → b :=
-sorry
+λa b, λf, f (λg, g (λha, f (λh, ha)))
 
 /-! 2.2 (2 points). Prove the same Fokkink lemma again, this time by providing a
 structured proof, with `assume`s and `show`s. -/
 
 lemma weak_peirce₃ :
   ∀a b : Prop, ((((a → b) → a) → a) → b) → b :=
-sorry
+fix a b,
+assume f,
+show b, from f (
+  assume g,
+  show a, from g (
+    assume ha,
+    show b, from f (
+      assume h,
+      ha
+    )
+  )
+)
 
 end LoVe
